2f4ae09e126f8d98223dc1ad9cc039c55358d5bd
Wilhelm Gren <wilhelmgren@outlook.com>
2024-09-29 12:47:41 +0200

    SQL Query builder
    Very simple query builder implementation that's not very flexible so far

--- entity.go
+++ entity.go
@@ -3,13 +3,56 @@ package gyr
 import (
 	"errors"
 	"reflect"
+	"slices"
+	"strconv"
 	"strings"
 )
 
+// Metadata for a registered entity. Table has to be set, everything else is optional. Columns can be used instead of gyr_column tags on struct fields.
 type EntityMetadata struct {
 	Table string
 	// Is overwritten by RegisterEntity if a field with a gyr_column tag is detected in the struct being registered
 	Columns []string
+}
+
+const (
+	queryType           = 1
+	queryColumns        = 1 << 1
+	queryIsInConditions = 1 << 2
+	queryHasValueAdded  = 1 << 3
+)
+
+type BaseQueryBuilder interface {
+	// Get the SQL Query in its current state from the builder
+	Query() string
+}
+
+type QueryBuilder[EntityType any] struct {
+	sb             *strings.Builder
+	entityMetadata EntityMetadata
+	fieldsSet      int
+}
+
+type SelectBuilder interface {
+	BaseQueryBuilder
+	// Start adding WHERE-conditions to your query.
+	Where(string) WhereBuilder
+}
+
+type InsertBuilder interface {
+	BaseQueryBuilder
+	// Add a set of values to the INSERT-query
+	AddValue() InsertBuilder
+}
+
+type WhereBuilder interface {
+	BaseQueryBuilder
+	// Equals condition with a SQL template variable
+	EqualsVar() WhereBuilder
+	// Equals a set value
+	EqualsValue(any) WhereBuilder
+	And(string) WhereBuilder
+	Or(string) WhereBuilder
 }
 
 var (
@@ -20,49 +63,167 @@ const (
 	gyr_column_tag = "gyr_column"
 )
 
+// Get a query builder instance. The entity must be registered using RegisterEntity.
+func NewQuery[EntityType any]() *QueryBuilder[EntityType] {
+	metadata, err := getEntityMetadata[EntityType]()
+	if err != nil {
+		return nil
+	}
+	return &QueryBuilder[EntityType]{
+		sb:             &strings.Builder{},
+		entityMetadata: metadata,
+	}
+}
+
+func (qb *QueryBuilder[EntityType]) Query() string {
+	return qb.sb.String()
+}
+
+func (qb *QueryBuilder[EntityType]) SelectAll() SelectBuilder {
+	return qb.Select(qb.entityMetadata.Columns)
+}
+
+func (qb *QueryBuilder[EntityType]) Select(columns []string) SelectBuilder {
+	if qb.fieldsSet&queryType > 0 {
+		panic("query type already set")
+	}
+	for _, column := range columns {
+		if !qb.hasColumn(column) {
+			panic("Unknown column: " + column)
+		}
+	}
+
+	qb.sb.WriteString("select ")
+	qb.sb.WriteString(strings.Join(columns, ", "))
+	qb.sb.WriteString(" from ")
+	qb.sb.WriteString(qb.entityMetadata.Table)
+	qb.fieldsSet |= queryType
+	return qb
+}
+
+// Create an INSERT-query using all registered columns.
+func (qb *QueryBuilder[EntityType]) InsertAll() InsertBuilder {
+	return qb.Insert(qb.entityMetadata.Columns)
+}
+
+// Create an INSERT-query with a subset of all columns.
+func (qb *QueryBuilder[EntityType]) Insert(columns []string) InsertBuilder {
+	if qb.fieldsSet&queryType > 0 {
+		panic("query type already set")
+	}
+
+	for _, column := range columns {
+		if !qb.hasColumn(column) {
+			panic("Unknown column: " + column)
+		}
+	}
+
+	qb.sb.WriteString("insert into ")
+	qb.sb.WriteString(qb.entityMetadata.Table)
+	qb.sb.WriteString(" (")
+	qb.sb.WriteString(strings.Join(columns, ", "))
+	qb.sb.WriteString(") values ")
+	qb.entityMetadata.Columns = columns
+	return qb
+}
+
+func (qb *QueryBuilder[EntityType]) AddValue() InsertBuilder {
+	if qb.fieldsSet&queryHasValueAdded > 0 {
+		qb.sb.WriteRune(',')
+	}
+	qb.sb.WriteRune('(')
+	qb.sb.WriteString(nVars(len(qb.entityMetadata.Columns)))
+	qb.sb.WriteRune(')')
+	qb.fieldsSet |= queryHasValueAdded
+	return qb
+}
+
+func (qb *QueryBuilder[EntityType]) Where(column string) WhereBuilder {
+	if qb.fieldsSet&queryType == 0 {
+		panic("no query type set")
+	}
+	if !qb.hasColumn(column) {
+		panic("Unknown column: " + column)
+	}
+
+	qb.sb.WriteString(" where ")
+	qb.sb.WriteString(column)
+	qb.fieldsSet |= queryIsInConditions
+	return qb
+}
+
+func (qb *QueryBuilder[EntityType]) And(column string) WhereBuilder {
+	if qb.fieldsSet&queryIsInConditions == 0 {
+		panic("QueryBuilder is not in conditions phase")
+	}
+	if !qb.hasColumn(column) {
+		panic("Unknown column: " + column)
+	}
+
+	qb.sb.WriteString(" and ")
+	qb.sb.WriteString(column)
+	return qb
+}
+
+func (qb *QueryBuilder[EntityType]) EqualsVar() WhereBuilder {
+	return qb.EqualsValue("?")
+}
+
+func (qb *QueryBuilder[EntityType]) EqualsValue(value any) WhereBuilder {
+	if qb.fieldsSet&queryIsInConditions == 0 {
+		panic("QueryBuilder is not in conditions phase")
+	}
+	qb.sb.WriteString(" = ")
+	writeBasedOnType(qb.sb, value)
+	return qb
+}
+
+func (qb *QueryBuilder[EntityType]) Or(column string) WhereBuilder {
+	if qb.fieldsSet&queryIsInConditions == 0 {
+		panic("QueryBuilder is not in conditions phase")
+	}
+	if !qb.hasColumn(column) {
+		panic("Unknown column: " + column)
+	}
+
+	qb.sb.WriteString(" or ")
+	qb.sb.WriteString(column)
+	return qb
+}
+
+func (qb QueryBuilder[EntityType]) hasColumn(columnName string) bool {
+	return slices.Contains(qb.entityMetadata.Columns, columnName)
+}
+
+// Register an entity in the Gyr entity registry. Needs to be done in order to use the SQL helper methods in the Gyr library.
 func RegisterEntity[EntityType any](metadata EntityMetadata) {
 	entityType := reflect.TypeFor[EntityType]()
+
+	if metadata.Table == "" {
+		panic("no table defined for entity " + entityType.Name())
+	}
 	if detectedColumns := getColumnsFromType(entityType); len(detectedColumns) > 0 {
 		metadata.Columns = detectedColumns
 	}
 	entityRegistry[entityType] = metadata
 }
 
+// Helper method for creating a SELECT * query without any conditions
 func CreateSelectAllQuery[EntityType any]() (string, error) {
-	metadata, err := getEntityMetadata[EntityType]()
-	if err != nil {
-		return "", err
+	query := NewQuery[EntityType]()
+	if query == nil {
+		return "", errors.New("unknown entity type")
 	}
-	query := strings.Builder{}
-	query.WriteString("select * from ")
-	query.WriteString(metadata.Table)
-	return query.String(), nil
+	return query.SelectAll().Query(), nil
 }
 
+// Helper method for creating an insert for a single instance of an entity using all columns
 func CreateInsertQuery[EntityType any]() (string, error) {
-	metadata, err := getEntityMetadata[EntityType]()
-	if err != nil {
-		return "", err
+	query := NewQuery[EntityType]()
+	if query == nil {
+		return "", errors.New("unknown entity type")
 	}
-	query := strings.Builder{}
-	query.WriteString("insert into ")
-	query.WriteString(metadata.Table)
-	query.WriteRune('(')
-
-	maxColumnIndex := len(metadata.Columns) - 1
-	for i, columnName := range metadata.Columns {
-		query.WriteString(columnName)
-		if i < maxColumnIndex {
-			query.WriteRune(',')
-		}
-	}
-
-	query.WriteRune(')')
-	query.WriteString(" values (")
-	query.WriteString(nVars(len(metadata.Columns)))
-	query.WriteRune(')')
-
-	return query.String(), nil
+	return query.InsertAll().AddValue().Query(), nil
 }
 
 func getColumnsFromType(entityType reflect.Type) []string {
@@ -89,4 +250,19 @@ func getEntityMetadata[EntityType any]() (EntityMetadata, error) {
 
 func nVars(n int) string {
 	return strings.Repeat("?,", n)[:(n*2)-1]
+}
+
+func writeBasedOnType(sb *strings.Builder, value any) {
+	switch v := value.(type) {
+	case string:
+		if v == "?" {
+			sb.WriteString(v)
+		} else {
+			sb.WriteRune('\'')
+			sb.WriteString(v)
+			sb.WriteRune('\'')
+		}
+	case int:
+		sb.WriteString(strconv.Itoa(v))
+	}
 }
--- entity_test.go
+++ entity_test.go
@@ -37,6 +37,18 @@ func TestDetectColumns(t *testing.T) {
 	}
 }
 
+func TestCreateSelectAll(t *testing.T) {
+	RegisterEntity[TestEntity](EntityMetadata{Table: "test_entity_table"})
+	query, err := CreateSelectAllQuery[TestEntity]()
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if query != "select name, count from test_entity_table" {
+		t.Fail()
+	}
+}
+
 func TestCreateInsert(t *testing.T) {
 	RegisterEntity[TestEntity](EntityMetadata{Table: "test_entity_table"})
 	insertQuery, err := CreateInsertQuery[TestEntity]()
@@ -44,7 +56,45 @@ func TestCreateInsert(t *testing.T) {
 		t.Log(err)
 		t.Fail()
 	}
-	if insertQuery != "insert into test_entity_table(name,count) values (?,?)" {
+	if insertQuery != "insert into test_entity_table (name, count) values (?,?)" {
 		t.Fail()
 	}
+}
+
+func TestMultiInsertBuilder(t *testing.T) {
+	RegisterEntity[TestEntity](EntityMetadata{Table: "test_entity_table"})
+	query := NewQuery[TestEntity]().Insert([]string{"name", "count"}).AddValue().AddValue().AddValue().Query()
+	if query != "insert into test_entity_table (name, count) values (?,?),(?,?),(?,?)" {
+		t.Fail()
+	}
+}
+
+func TestSelectBuilderPanics(t *testing.T) {
+	RegisterEntity[TestEntity](EntityMetadata{Table: "test_entity_table"})
+	qb := NewQuery[TestEntity]()
+	qb.SelectAll()
+	defer func() {
+		if nilIfNoRecover := recover(); nilIfNoRecover == nil {
+			t.Fail()
+		}
+	}()
+	qb.SelectAll()
+}
+
+func TestSelectBuilderWhere(t *testing.T) {
+	RegisterEntity[TestEntity](EntityMetadata{Table: "test_entity_table"})
+	qb := NewQuery[TestEntity]()
+	query := qb.SelectAll().Where("name").EqualsValue("kalle karlsson").And("count").EqualsVar().Query()
+	if query != "select name, count from test_entity_table where name = 'kalle karlsson' and count = ?" {
+		t.Fail()
+	}
+}
+
+func TestRegisterEntityPanics(t *testing.T) {
+	defer func() {
+		if recoveredError := recover(); recoveredError != "no table defined for entity TestEntity" {
+			t.Fail()
+		}
+	}()
+	RegisterEntity[TestEntity](EntityMetadata{})
 }
